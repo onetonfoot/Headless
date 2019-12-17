51f20e7e92ec32ece12f740fcee2365349d04ce4
Dominic Morgan <dominic.mark.morgan@gmail.com>
2019-12-17 22:31:31 +0800

    add kill port

--- src/Headless.jl
+++ src/Headless.jl
@@ -3,10 +3,12 @@ module Headless
 using Signals
 import JSON
 
-export Browser, Protocol
+export Browser, Protocol, kill_port
 
 include("protocol/Protocol.jl")
 include("browser/Browser.jl")
+include("utils.jl")
+
 
 using .Browser
 export Chrome, close, opentab!, closetab!, activatetab!, tabnames
---
+++ src/utils.jl
@@ -0,0 +1,23 @@
+using Base.Sys: islinux, isapple
+
+function kill_port()
+
+    cmd = if islinux()
+        `lsof -i4`
+    else
+        `lsof -nP -i4`
+    end
+
+    try
+        cmd = pipeline(cmd, `grep :9222`)
+        result = read(cmd, String) |>  rstrip |> x -> split(x, "\n")
+
+        for line in result
+            pid = split(line)[2]
+            run(`kill -9 $pid`)
+            print("Killed $pid")
+        end
+    catch
+        print("Nothing to kill")
+    end
+end
